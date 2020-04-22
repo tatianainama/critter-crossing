a61a3fc187aafbdd49f8fc3b6770fff9d29478e8
Tatiana Inama <tatii.oxo@gmail.com>
2020-04-22 13:23:42 +0200

    Add Table to insect container

--- app/src/Insects/index.tsx
+++ app/src/Insects/index.tsx
@@ -1,8 +1,25 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import { Insect } from 'types';
+import { getInsects } from 'services/Api';
+import Table from 'components/Table';
 
 const InsectTable: FunctionComponent = () => {
+  const [ data, setData ] = useState<Insect[]>([]);
+  
+  const fetchData = async () => {
+    const response = await getInsects()
+    setData(response)
+  }
+  
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
   return (
-    <div>insect</div>
+    <>
+      <Table data={data}></Table>
+    </>
+  
   )
 };
 
--- app/src/services/Api.ts
+++ app/src/services/Api.ts
@@ -3,7 +3,7 @@ import { Fish, Insect } from 'types';
 
 export const getFishes = (): Promise<Fish[]> => Axios.get(`${process.env.REACT_APP_API}/data/fishes.json`).then(result => result.data);
 
-export const getInsects = (): Promise<Insect[]> => Axios.get(`${process.env.REACT_APP_API}/insects`).then(result => result.data);
+export const getInsects = (): Promise<Insect[]> => Axios.get(`${process.env.REACT_APP_API}/data/insects.json`).then(result => result.data);
 
 export default {
   getFishes,
